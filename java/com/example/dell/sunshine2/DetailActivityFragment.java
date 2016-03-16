65ad5d96efb01558b2ded979b56017e3c672123b
EsraaEidHassan <esraa.eidhassan@gmail.com>
2016-03-16 03:27:27 +0300

    Added files via upload

---
+++ java/com/example/dell/sunshine2/DetailActivityFragment.java
@@ -0,0 +1,45 @@
+package com.example.dell.sunshine2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        View rootView = inflater.inflate(R.layout.detail, container, false);
+        Intent intent=getActivity().getIntent();
+            if(intent!=null&&intent.hasExtra(Intent.EXTRA_TEXT)){
+                String forecastStr=intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView)rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+            }
+        return rootView;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
